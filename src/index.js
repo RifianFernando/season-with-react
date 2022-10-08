6b52dbbc4291a64b6f76de7b8ea3da2fe96bd7e5
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-10-08 22:05:15 +0700

    done

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class App extends React.Component{
         );
     }
     render(){
-        return <div>Latitude: {this.state.lat} inin function looping</div>;
+        return <div>Latitude: {this.state.lat} sudah berhasil</div>;
     }
 }
 
