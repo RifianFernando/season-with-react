a848e06374a4720afac7b6afe8fbaab75683f76b
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-10-12 16:55:52 +0700

    [add passing state as props, lifecycle method function, conditional rendering method]

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-const App = () => {
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    }else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+
+}
+//passing props with const element
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'summer' ? 'Its hottt' : 'burrr';
+
     return (
-        <div>SeasonDisplay</div>
+        <div>SeasonDisplay {text}</div>
     );
 };
+
+//passing props with class component
+// class SeasonDisplay extends React.Component{
+//     render(){
+//         return <h1>Hello, {this.props.lat}</h1>
+//     }
+// }
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {lat:null};    
+    // constructor(props){
+    //     super(props);
+    //     this.state = {lat:null};   
+        
+    // }
+    state = {lat:null, errorMessage: ''};
 
-        // window.navigator.geolocation.getCurrentPosition(
-        //     (position) => console.log(position),
-        //     (err) => console.log(err)
-        // );
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => console.log(err)
+            (position) => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+
+    componentDidUpdate(){
+        console.log('My componentwas just updated');
+    }
+    
     render(){
-        return <div>Latitude: {this.state.lat} sudah berhasil</div>;
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        // else if(!this.state.errorMessage && this.state.lat){
+        //     return <div>Latitude: {this.state.lat}</div>;
+        // }
+        else if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} err={this.state.errorMessage}/>;
+        }
+        else{
+            return <div>Loading!!</div>
+        }
+        
+        //return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
