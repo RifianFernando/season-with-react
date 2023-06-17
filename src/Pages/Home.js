843b8280a256838a06f338a700fa44452a408404
Rifian Fernando <rifianfernando19@gmail.com>
2023-06-17 23:44:24 +0700

    feat(useState): add useState for radio button selected value

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input } from 'technotic'
+import { Input, Button } from 'technotic'
 import { Radio } from 'antd';
 
 
@@ -47,6 +47,27 @@ function Home() {
         setValue4(value);
     };
 
+    // const SubmitForm = (e) => {
+    //     e.preventDefault();
+    //     const inputValue = e.target.elements.inputField.value;
+    //     console.log('Input value:', inputValue);
+    //     console.log('submit');
+    // }
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleInputChange = (e) => {
+        setSelectedOption(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const inputValue = e.target.elements.inputField.value;
+        const selectedOption = e.target.elements.radioGroup.value;
+        console.log('Text Input Value:', inputValue);
+        console.log('Selected Radio Button Value:', selectedOption);
+        console.log('Submit');
+    };
     return (
         <div>
             {/* <Radio></Radio>
@@ -75,16 +96,41 @@ function Home() {
             {/* <Radio>
                 Test asdfas
             </Radio> */}
-            <Radio.Group options={plainOptions} onChange={onChange1} value={value1} />
-            <br />
-            <Input.Radio
-                radioType="secondary"
-                // defaultChecked={true}
-                onChange={handleRadioChange}
-                onClick={handleRadioClick}
-            >
-                Test asdfas
-            </Input.Radio>
+
+
+            {/* <form onSubmit={SubmitForm}>
+                <input type='text' />
+                <Input.Radio
+                    value='test'
+                >
+                    Test asdfas
+                </Input.Radio>
+
+                <input type='radio' value={'1'} />
+
+                <Button type="submit">
+                    Submit
+                </Button>
+            </form> */}
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="inputField" />
+
+                <Input.Radio
+                    value='test'
+                    name='radioGroup'
+                >
+                    Test asdfas
+                </Input.Radio>
+
+                <Radio
+                    value='test'
+                    name='radioGroup'>
+                    Test ant design
+                </Radio>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
